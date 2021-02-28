a90ae6442f5f8be753af43d9220f2dad0fc5b4d5
Jakob <jakoblundbergsu15@gmail.com>
2021-02-28 15:22:44 +0100

    LocalDateTimeDisplay Test

---
+++ src/main/java/se/newton/springbootdemo/LocalDateTimeDisplay.java
@@ -0,0 +1,17 @@
+package se.newton.springbootdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class LocalDateTimeDisplay
+{
+    @RequestMapping (method = RequestMethod.GET, path = "/time")
+    public String displayCurrentTime (){
+        return LocalDateTime.now().toString();
+    }
+
+}
